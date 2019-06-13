35bf6a69483902865095a630956a56f54587f4b1
Alexandr Starovoitov <stalex.info@yandex.ru>
2019-06-14 00:13:00 +0300

    test upload file in event

--- app/files/views.py
+++ app/files/views.py
@@ -6,8 +6,18 @@ from . import files
 
 @files.route('/upload', methods=('POST',))
 def upload():
+    WIDTH = 1200
+    HEIGHT = 800
+
     file_ = request.files.get('file')
     if file_ is not None:
-        im = Image.open(file_)
+        try:
+            with Image.open(file_) as im:
+                print(file_, im.format, "%dx%d" % im.size, im.mode)
+                im.thumbnail((WIDTH, HEIGHT), Image.ANTIALIAS)
+                print(file_, im.format, "%dx%d" % im.size, im.mode)
+                im.show()
+        except IOError:
+            pass
 
     return jsonify(status=1, data={'id': 1, 'filename': 'rrt'}), 200
