bd6714d39b71390eb4de6e44b88ee9a328bb5b31
Alexandr Starovoitov <stalex.info@yandex.ru>
2019-06-14 11:42:51 +0300

    Удаление  фото в событие

--- app/events/ajax.py
+++ app/events/ajax.py
@@ -12,6 +12,10 @@ from . import events
 @events.route('/ajax/bind/eventfile', methods=('POST',))
 def bind_event_and_photo():
 
+    """
+    Метод для добавления связей фото и события
+    """
+
     if request.is_xhr:
 
         event_id = request.form.get('event_id')
@@ -36,6 +40,44 @@ def bind_event_and_photo():
                     )
 
                     db.session.add(event_file)
+                    db.session.commit()
+
+                return jsonify(status=1, massage='OK')
+
+        except (ValueError, TypeError):
+            return jsonify(status=-1, massage='Не корректные данные')
+
+        else:
+            return jsonify(status=-1, massage='Не корректные данные')
+    else:
+        return jsonify(status=-1, massage='Данный метод можно вызвать только через ajax')
+
+
+@events.route('/ajax/unbind/eventfile', methods=('POST',))
+def unbind_event_and_photo():
+
+    """
+    Метод для удаления связей фото и события
+    """
+
+    if request.is_xhr:
+
+        event_id = request.form.get('event_id')
+        file_id = request.form.get('file_id')
+
+        try:
+
+            if event_id and file_id:
+
+                event_id = int(event_id)
+                file_id = int(file_id)
+
+                del_file = EventFiles.query.filter(
+                    and_(EventFiles.event_id.__eq__(event_id),
+                         EventFiles.file_id.__eq__(file_id))).first()
+
+                if del_file is not None:
+                    db.session.delete(del_file)
                     db.session.commit()
 
                 return jsonify(status=1, massage='OK')
